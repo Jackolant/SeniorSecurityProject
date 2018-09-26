5f6dece506ed19f326bee9140b09dca33dc60278
austinthale <bbqdpickles@gmail.com>
2018-09-26 11:43:39 -0600

    Add NavMeshAgent and script to move to goal

---
+++ Senior Security Project/Assets/Scripts/MoveDestination.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveDestination : MonoBehaviour {
+    public Transform goal;
+
+    void Start()
+    {
+        StartCoroutine(Move(goal.position));
+    }
+
+    IEnumerator Move (Vector3 position)
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        
+        while (gameObject.activeInHierarchy)
+        {
+            agent.destination = position;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
+
+
+// TODO:
+// onCollisionEnter, agent.stop or similar method.
