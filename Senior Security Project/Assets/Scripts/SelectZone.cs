00a391c14a5df3c7ae9553566cb3e92f30fd7761
Jacob Ressler <jacobrressler@gmail.com>
2018-10-08 12:48:41 -0600

    add stuff

---
+++ Senior Security Project/Assets/Scripts/SelectZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectZone : MonoBehaviour
+{
+
+    public bool selected = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!selected)
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            selected = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+            selected = false;
+        }
+    }
+}
